7be646e237a71c77b61651df2a7bae8a78f03d2b
ddesioeleva <d.desio@eleva.it>
2025-01-02 22:45:41 +0100

    Add AI Kit

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,16 +1,25 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    }).authorization(allow => [allow.owner()]),
+  chat: a.conversation({
+    aiModel: a.ai.model('Amazon Nova Micro'),
+    systemPrompt: 'You are a helpful assistant',
+  }).authorization((allow) => allow.owner()),
+  generateRecipe: a.generation({
+    aiModel: a.ai.model('Amazon Nova Micro'),
+    systemPrompt: 'You are a helpful assistant that generates recipes.',
+  })
+      .arguments({
+        description: a.string(),
+      })
+      .returns(
+          a.customType({
+            name: a.string(),
+            ingredients: a.string().array(),
+            instructions: a.string(),
+          })
+      )
+      .authorization((allow) => allow.authenticated()),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -27,7 +36,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,24 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from "@aws-amplify/ui-react";
+import { AIConversation } from '@aws-amplify/ui-react-ai';
+import { useAIConversation } from './client';
 
-const client = generateClient<Schema>();
+export default function App() {
+    const [
+        {
+            data: { messages },
+            isLoading,
+        },
+        handleSendMessage,
+    ] = useAIConversation('chat');
+    // 'chat' is based on the key for the conversation route in your schema.
 
-function App() {
-  const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
-  return (
-    <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li
-              onClick={() => deleteTodo(todo.id)}
-              key={todo.id}>
-              {todo.content}
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
-  );
+    return (
+        <Authenticator>
+            <AIConversation
+                messages={messages}
+                isLoading={isLoading}
+                handleSendMessage={handleSendMessage}
+            />
+        </Authenticator>
+    );
 }
-
-export default App;
