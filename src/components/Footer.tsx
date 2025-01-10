3da7f3ec1ce268e48b435cdbd61e5262e4e02897
ddesioeleva <d.desio@eleva.it>
2025-01-10 10:01:39 +0100

    Add disclaimer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import {View} from "@aws-amplify/ui-react";
 const Footer = () => {
     return (
         <View className="footer">
-            &copy; All Rights Reserved.
+            &copy; All Rights Reserved.<br/>
+            <div className="warning">Attenzione: i dati transitano e<br/> vengono salvati in N.Virginia, USA.</div>
             Made with ❤️ by <a href="https://eleva.it" target="_blank">Eleva</a>
         </View>
     );
