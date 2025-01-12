6af0a27169d098bc07ba4f34153f04a56091e6a1
ddesioeleva <d.desio@eleva.it>
2025-01-12 20:04:24 +0100

    Add logout function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,34 @@
 import {View} from "@aws-amplify/ui-react";
+import {signOut} from "@aws-amplify/auth";
+import { getCurrentUser } from 'aws-amplify/auth';
+import {useEffect, useState} from "react";
 
 const Header = () => {
+
+    const [isLogged, setLogged] = useState(false);
+    const [loginId, setLoginId] = useState('');
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try {
+                const { signInDetails } = await getCurrentUser();
+                if(signInDetails && signInDetails.loginId){
+                    setLoginId(signInDetails.loginId);
+                }
+                setLogged(true);
+            } catch (error) {
+                setLoginId('');
+                setLogged(false);
+            }
+        };
+
+        fetchUserDetails();
+    }, []);
+
+    const handleSignOut = async () => {
+        await signOut()
+    }
+
     return (
         <View className="header hint">
             <a href="https://eleva.it" target="_blank">
@@ -9,6 +37,18 @@ const Header = () => {
                      src="https://eleva.it/assets/imgs/eleva-logo-white.svg"
                 />
             </a>
+            {isLogged &&
+                <>
+                    {loginId &&
+                        <span id="user-name">
+                            {loginId}
+                        </span>
+                    }
+                    <button id="sign-out" type="button" onClick={handleSignOut}>
+                        Sign out
+                    </button>
+                </>
+            }
         </View>
     );
 };
